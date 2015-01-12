bf7a9a22451bd18ea6702e4831e895d887288714
Oliver Gloth <ogloth@herring.todtnau.engits.de>
2015-01-12 20:53:01 +0100

    added max and min constants for edl::real

--- edl/edl.h
+++ edl/edl.h
@@ -3,12 +3,18 @@
 
 #define EDL_NAMESPACE edl
 
+#include <cfloat>
+
 namespace EDL_NAMESPACE
 {
 #ifdef REAL_FLOAT
   typedef float real;
+  const real max_real = FTL_MAX;
+  const real min_real = FTL_MIN;
 #else
   typedef double real;
+  const real max_real = DBL_MAX;
+  const real min_real = DBL_MIN;
 #endif
 }
 
