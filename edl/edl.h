c60b4d2e2ba78e6da4f375cd81e56b5820aa4029
Oliver Gloth <ogloth@engits.com>
2015-04-23 18:54:48 +0200

    added sign, sign0, and sqr

--- edl/edl.h
+++ edl/edl.h
@@ -21,6 +21,25 @@ namespace EDL_NAMESPACE
   const real min_real   = -DBL_MAX;
   const real small_real =  DBL_MIN;
 #endif
+
+  inline real sign(real x)
+  {
+    if (x >= 0) return  1;
+    return -1;
+  }
+
+  inline real sign0(real x)
+  {
+    if      (x > 0) return  1;
+    else if (x < 0) return -1;
+    return 0;
+  }
+
+  inline real sqr(real x)
+  {
+    return x*x;
+  }
+
 }
 
 #endif
