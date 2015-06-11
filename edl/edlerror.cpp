cef63a3e459c7273b4441d19e328084a347040c3
Oliver Gloth <ogloth@engits.com>
2015-06-11 10:33:41 +0200

    aplied licence header to all files

--- edl/edlerror.cpp
+++ edl/edlerror.cpp
@@ -1,29 +1,24 @@
-// !!
-// This is a part of MOUSE, a library for PDE's on unstructured grids
-// Copyright (C) 1999 Oliver Gloth <oliver@vug.uni-duisburg.de>
-// Institut fuer Verbrennung und Gasdynamik
-// Universitaet Duisburg, Germany
-// Institute for Combustion and Gas Dynamics
-// University of Duisburg, Germany
-// Tue Oct 26 1999
-//
-// please see http://www.vug.uni-duisburg.de/MOUSE for more information
-// please send any questions or suggestions to mouse@www.vug.uni-duisburg.de
-//  
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software 
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-// !!
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                    +
+// + This file is part of enGitsDataLib.                                +
+// +                                                                    +
+// + Copyright 2015 enGits GmbH                                         +
+// +                                                                    +
+// + enGitsDataLib is free software: you can redistribute it and/or     +
+// + modify it under the terms of the GNU Lesser General Public License +
+// + as published by the Free Software Foundation, either version 3 of  +
+// + the License, or (at your option) any later version.                +
+// +                                                                    +
+// + enGitsDataLib is distributed in the hope that it will be useful,   +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of     +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      +
+// + GNU Lesser General Public License for more details.                +
+// +                                                                    +
+// + You should have received a copy of the GNU Lesser General Public   +
+// + License along with enGitsDataLib.                                  +
+// + If not, see <http://www.gnu.org/licenses/>.                        +
+// +                                                                    +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #include "edlerror.h"
 #include <cstdlib>
--- edl/mathvector_operators.h
+++ edl/mathvector_operators.h
@@ -1,23 +1,24 @@
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// +                                                                      +
-// + This file is part of enGrid.                                         +
-// +                                                                      +
-// + Copyright 2008-2014 enGits GmbH                                      +
-// +                                                                      +
-// + enGrid is free software: you can redistribute it and/or modify       +
-// + it under the terms of the GNU General Public License as published by +
-// + the Free Software Foundation, either version 3 of the License, or    +
-// + (at your option) any later version.                                  +
-// +                                                                      +
-// + enGrid is distributed in the hope that it will be useful,            +
-// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
-// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
-// + GNU General Public License for more details.                         +
-// +                                                                      +
-// + You should have received a copy of the GNU General Public License    +
-// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
-// +                                                                      +
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                    +
+// + This file is part of enGitsDataLib.                                +
+// +                                                                    +
+// + Copyright 2015 enGits GmbH                                         +
+// +                                                                    +
+// + enGitsDataLib is free software: you can redistribute it and/or     +
+// + modify it under the terms of the GNU Lesser General Public License +
+// + as published by the Free Software Foundation, either version 3 of  +
+// + the License, or (at your option) any later version.                +
+// +                                                                    +
+// + enGitsDataLib is distributed in the hope that it will be useful,   +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of     +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      +
+// + GNU Lesser General Public License for more details.                +
+// +                                                                    +
+// + You should have received a copy of the GNU Lesser General Public   +
+// + License along with enGitsDataLib.                                  +
+// + If not, see <http://www.gnu.org/licenses/>.                        +
+// +                                                                    +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #include <typeinfo>
 
 // asssignements
